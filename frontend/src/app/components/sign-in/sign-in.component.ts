a0f18cc484232a888456a5d822645a92635a51e6
StevenSsj1 <lllano@est.ups.edu.ec>
2023-07-27 23:08:29 -0500

    Version Final1.1

--- frontend/src/app/components/sign-in/sign-in.component.ts
+++ frontend/src/app/components/sign-in/sign-in.component.ts
@@ -13,6 +13,7 @@ export class SingInComponent implements OnInit {
     psw: ''
   }
 
+  camposLlenos = true;
     constructor(private ServiceAuth:AuthService, private router:Router){
 
   }
@@ -20,15 +21,30 @@ export class SingInComponent implements OnInit {
       
   }
 
- Ingreso(){
-  this.ServiceAuth.login(this.users).subscribe(
-    res => {
-      localStorage.setItem('auth_token', res.token)
-      this.router.navigate(['/admin'])
+  Ingreso() {
+    // Verificar si los campos están llenos antes de enviar la solicitud de inicio de sesión
+    if (this.users.correo.trim() === '' || this.users.psw.trim() === '') {
+      this.camposLlenos = false;
+      return; // Detener el envío del formulario si los campos están vacíos
     }
 
-  )
- }
+    // Realizar la llamada al servicio de autenticación (AuthService) para enviar las credenciales del usuario.
+    this.ServiceAuth.login(this.users).subscribe(
+      (res: any) => {
+        // El servidor responde con el token de autenticación (res.token).
+        // Almacenar el token en el localStorage para mantener la sesión.
+        localStorage.setItem('auth_token', res.token);
+
+        // Navegar a la ruta '/admin' después de iniciar sesión correctamente.
+        this.router.navigate(['/admin']);
+      },
+      (error: any) => {
+        // Manejar cualquier error que ocurra durante el proceso de inicio de sesión.
+        console.log('Error al iniciar sesión:', error);
+        // Puedes mostrar un mensaje de error al usuario si lo deseas.
+      }
+    );
+  }
 }
 
 // Ingreso() {
